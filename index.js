15db7aae96c8559fe2e43e24ecad25f9fb83cc96
SULAJSJS <chistotiktok@gmail.com>
2022-12-03 15:21:09 +0600

    первый commit

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import multer from 'multer';
+import { loginValidation, postCreateValidation, registerValidation } from './validations.js';
+import { UserController, PostController } from './controllers/index.js';
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+
+mongoose
+  .connect(
+    'mongodb+srv://Sula:sulamillion1000000@cluster0.xudv6sw.mongodb.net/blog?retryWrites=true&w=majority',
+  )
+  .then((result) => console.log('Connect to Database is success!'))
+  .catch((error) => console.log(error));
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.Login);
+
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.Register);
+
+app.get('/auth/me', checkAuth, UserController.GetMe);
+app.use('/uploads', express.static('uploads'));
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get('/posts', PostController.getAll);
+app.get('/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
+);
+
+app.listen(5000, (err) => {
+  if (err) {
+    return console.log(err.message);
+  } else {
+    console.log('Node server is successfully runned');
+  }
+});
---
+++ validations.js
@@ -0,0 +1,20 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+  body('email', 'Некоректная почта').isEmail(),
+  body('password', 'Некоректый пароль').isLength({ min: 5 }),
+  body('fullName', 'Неправильное имя или фамилия').isLength({ min: 3 }),
+  body('avatar', 'Неверная ссылка на фото профиля').optional().isURL(),
+];
+
+export const loginValidation = [
+  body('email', 'Некоректная почта').isEmail(),
+  body('password', 'Некоректый пароль').isLength({ min: 5 }),
+];
+
+export const postCreateValidation = [
+  body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
+  body('text', 'Введите текст статьи').isLength({ min: 3 }).isString(),
+  body('tags', 'Неверный формат тэгов').optional().isString(),
+  body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+];
